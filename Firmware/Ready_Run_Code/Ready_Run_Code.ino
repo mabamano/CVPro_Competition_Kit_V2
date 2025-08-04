6d7979806fc296be67f0395ff7a18b1dc7a8eef4
robotixdevteam <development@merituseducation.com>
2025-08-04 14:08:23 +0530

    Git Update

--- Firmware/Ready_Run_Code/Ready_Run_Code.ino
+++ Firmware/Ready_Run_Code/Ready_Run_Code.ino
@@ -1,13 +1,13 @@
 // Function List // #######################################################################################//
 /*
-1. get_CS_Value(); - Use this to get Color Sensor Value.
-2. get_US_Value("FUS"); - (FUS, F1US, F2US, BUS, RUS, LUS)
-3. rgb_led(150, 0, 150); - (R, G, B)
-4. motor_forward(220); Speed Limit - (190 - 255)
-5. motor_backward(220); Speed Limit - (190 - 255)
-6. motor_stop();
-7. set_servo_to(100); Angle(Center - 100, Left - 80, Right - 120)
-8. get_PushButton_Value(); Values(0 or 1)
+1. get_PushButton_Value(); - Function used to get the push button state. Returns either 0 (not pressed) or 1 (pressed).
+2. get_CS_Value(); - Function used to retrieve the value from the color sensor.
+3. get_US_Value("FUS"); - Function used to get the value from an ultrasonic sensor. Replace "FUS" with any of the following to access the respective sensor: "FUS", "F1US", "F2US", "BUS", "RUS", or "LUS".
+4. rgb_led(150, 0, 150); - Function used to light up the RGB LED by specifying red, green, and blue intensity values.
+5. motor_forward(220); - Function used to move the DC motor in the forward direction. Speed can be set between 190 and 255.
+6. motor_backward(220); - Function used to move the DC motor in the backward direction. Speed can be set between 190 and 255.
+7. motor_stop(); - Function used to stop the DC motor.
+8. set_servo_to(100); - Function used to set the angle of the servo motor. Standard angles: Center = 100, Left = 80, Right = 120.
 */
 //########################################################################################################//
 bool DPDT_STATE = 0; 
